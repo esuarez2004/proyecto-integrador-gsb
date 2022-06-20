fbae070b3b43ca5ded6581ff04cf228502fbbbd8
emagondell <egondell@udesa.edu.ar>
2022-06-20 15:51:59 -0300

    index

--- js/index.js
+++ js/index.js
@@ -2,15 +2,20 @@ let proxi = " https://cors-anywhere.herokuapp.com/";
 let endpoint = "https://api.deezer.com/chart";
 let url = proxi+endpoint
 
-fetch(url)
-    .then(function (response) {
-        return response.json
-    } )
-    .then(function (data){
-        console.log(data);
-        let canciones = data.tracks;
-        let albumes = data.albums;
-        let artistas = data.artist;
+.fetch (url)
+    .then (function(response){
+        return response.json();
+    }) 
+    .then (function(datos) {
+
+    
+    
+        console.log(datos);
+        let canciones = datos.tracks.data;
+        let albumes = datos.albums.data;
+        let artistas = datos.artist.data;
+    
+       
        
 
         let sectionCanciones = document.querySelector('.cajapadre1');
@@ -18,39 +23,36 @@ fetch(url)
         let sectionArtistas = document.querySelector('.cajapadre3')
 
         for (let i = 0; 1 < 5; i++) {
-            sectionCanciones.innerHTML += ` <article class="canciones">
-                                            <a href="./detalle-canciones.html">
-                                            <img class="imagen" src="${canciones[i].cover_small}
-                                            </a>
-                                            <a class="titulo" href="./detalle-canciones.html">
-                                            <h4>${canciones[i].titel}</h4>
-                                            </a>
-                                            </article>
-                                            ` }
+            sectionCanciones.innerHTML += ` <article>
+                                        <img src="${canciones[i].cover_small}
+                                        <a href="./detalle-canciones.html?id=${canciones[i].id}">
+                                        <h4>${canciones[i].title}</h4>
+                                        <a href="./detalle-canciones.html?id=${canciones[i].artist.id}">
+                                        ${canciones[i].artist.name} </a>
+                                        </article>`};
+
 
 
         for (let i = 0; 1 < 5; i++){
-            sectionAlbumes.innerHTML += `<article class="albumes">
-                                        <a href="./detalle-album.html">
-                                        < img class="imagen" src="${albums[i].cover_small}
-                                        </a>
-                                        <a class= "titulo" href="./detalle-album.html">
+            sectionAlbumes.innerHTML += `<article>
+                                        < img src="${albums[i].cover_small}">
+                                        <a href="./detalle-album.html.id=${albums[i].id}">
                                         <h4>${albums[i].title}</h4>
                                         </a>
                                         </article>
                                         ` }
                                     
-    for (let i = 0; 1 < 5; i++){
-        sectionArtistas.innerHTML += ` <article class="artistas">
+         for (let i = 0; 1 < 5; i++){
+        sectionArtistas.innerHTML += ` <article >
                                         <a href="./detalle-artista.html">
-                                        <img class="imagen" src="${tracks[i].picture.small}
+                                        <img src="${artistas[i].picture_small}
                                         </a>
-                                        <a class=" titulo" href="./detalle-artista.html">
-                                        <h4>${tracks[i].name}</h4>
+                                        <a  href="./detalle-artista.html?id=${artistas[i].id}">
+                                        <h4>${cantantes[i].name}</h4>
                                         </a>
-                                        <article>
-                                        `}
+                                        </article>
+                                         `}
 
-
-
-})
+                                       
+                                        
+    })
